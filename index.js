26dcdba655567fb0d891bd731f1f7aab87247c30
nileshpatil-15 <113256649+nileshpatil-15@users.noreply.github.com>
2023-04-17 17:25:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const Notification = (props) => {
   const { message, className, path } = props;
-  const containerClassName={`notification ${className}`}
+  const containerClassName=`notification ${className}`
   return (
-    <div className={containerClassName}>
-      <img className="icon" src={path} />
-      <p className="message">{message}</p>
-    </div>
-  );
+      <div className={containerClassName}>
+      <img className='img' src=${path}/>
+      <p className='message'>${message}</p>
+
+      </div> 
+  )
+
 };
 
 const element = (
   <div className="inner-container">
-    <h1 className="heading">Notifications</h1>
-     { <Notification
-       message="Success Message"
-       className="Success-Message"
-       path="https://assets.ccbp.in/frontend/react-js/primary-icon-img.png" 
-    />  }
+    <h1 className="title">Notifications</h1>
+    <div className='notification-list-container'>
+    <Notification 
+    className ='primary-blue'
+    message='Information Message'
+    path='https://assets.ccbp.in/frontend/react-js/primary-icon-img.png'/>
+
+    </div>
   </div>
 );
 
